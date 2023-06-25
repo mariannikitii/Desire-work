ebcc398093ee229899b8a30310fb6387c4e4b360
mariannikitii <mariannikitii@gmail.com>
2023-06-25 13:57:53 +0300

    modified

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function () {
 
+ $('.header__btn').on('click', function() {
+	$('.rightsite-menu').removeClass('rightsite-menu--close')
+ });
+ $('.rightsite-menu__close').on('click', function() {
+	$('.rightsite-menu').addClass('rightsite-menu--close')
+ });
+
+ $('.top__slider').slick({
+	dots: true,
+	arrows: false,
+	fade: true,
+	autoplay: true
+	
+ });
 })
